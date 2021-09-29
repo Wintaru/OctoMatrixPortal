c88da1f7c1cd46190ea9aefce9dc4174f9c2ba6a
Josh <joshua.j.donner@gmail.com>
2021-09-29 14:32:47 -0500

    Read from DisplayLayerProgress plugin.

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
-import sys
+import microcontroller
 import time
 import board
 import terminalio
@@ -15,6 +15,7 @@ SCROLL_DELAY = 0.02
 SLEEP_DURATION = 5.0
 OCTOPI_SRC = secrets['octopi_host']
 JOB_DATA_SRC = OCTOPI_SRC + '/api/job'
+PLUGIN_DATA_SRC = OCTOPI_SRC + '/plugin/DisplayLayerProgress/values'
 SETTINGS_DATA_SRC = OCTOPI_SRC + '/api/settings' # Unused for now
 API_KEY = secrets['octopi_api_key']
 
@@ -100,44 +101,56 @@ print_left_str = ""
 progress_bar = ""
 
 while True:
-    # No need to hammer Octoprint for updates, every minute seems reasonable
-    if (not refresh_time) or (time.monotonic() - refresh_time) > 60:
-        # Fetch job data first
-        response = matrixportal.network.fetch(JOB_DATA_SRC, headers=HEADERS)
-        job_info = response.json()
-        response.close()
+    try:
+        # No need to hammer Octoprint for updates, every minute seems reasonable
+        if (not refresh_time) or (time.monotonic() - refresh_time) > 60:
+            # Fetch job data first
+            response = matrixportal.network.fetch(JOB_DATA_SRC, headers=HEADERS)
+            job_info = response.json()
+            response.close()
+            response = matrixportal.network.fetch(PLUGIN_DATA_SRC, headers=HEADERS)
+            layer_info = response.json()
+            response.close()
 
-        # This is usually encountered if you have the API key wrong
-        if 'error' in job_info:
-            now_printing_str = "Configuration Error (check your API key?)"
-            print_left_str = ""
-            progress_bar = ""
-        # If Octoprint is idle, this is what you get, some stuff is null
-        elif job_info['state'] != "Printing":
-            now_printing_str = "Not Printing"
-            print_left_str = ""
-            progress_bar = ""
-        # Hopefully you really are printing
-        else:
-            pt_left = job_info['progress']['printTimeLeft']
-            # It can take a while for Octoprint to come up with an estimate so we
-            # display 'Calculating' until this is ready to show
-            print_left_str = "Calculating"
-            if pt_left:
-                time_left = TimeStruct(pt_left)
-                print_left_str = ConvertTimeStructToDisplayString(time_left)
+            # This is usually encountered if you have the API key wrong
+            if 'error' in job_info:
+                now_printing_str = "Configuration Error (check your API key?)"
+                print_left_str = ""
+                progress_bar = ""
+            # If Octoprint is idle, this is what you get, some stuff is null
+            elif job_info['state'] != "Printing":
+                now_printing_str = "Not Printing"
+                print_left_str = ""
+                progress_bar = ""
+            # Hopefully you really are printing
+            else:
+                pt_left = job_info['progress']['printTimeLeft']
+                # It can take a while for Octoprint to come up with an estimate so we
+                # display 'Calculating' until this is ready to show
+                print_left_str = "Calculating"
+                if pt_left:
+                    time_left = TimeStruct(pt_left)
+                    print_left_str = ConvertTimeStructToDisplayString(time_left)
 
-            # prepare the strings for display and create progress bar
-            now_printing_str = "Now Printing: " + job_info['job']['file']['name']
-            progress_bar_percentage = (int)(job_info['progress']['completion'])
-            progress_bar = CreateProgressBar((int)(progress_bar_percentage / 10))
+                # prepare the strings for display and create progress bar
+                now_printing_str = "Now Printing: " + job_info['job']['file']['name']
+                progress_bar = str((int)(job_info['progress']['completion'])) + "%"
+                cur_layer = (int)(layer_info['layer']['current'])
+                tot_layer = (int)(layer_info['layer']['total'])
+                progress_bar += " " + str(cur_layer) + "/" + str(tot_layer)
 
-    refresh_time = time.monotonic()
+            refresh_time = time.monotonic()
 
-    # Update text
-    matrixportal.set_text(now_printing_str, 0)
-    matrixportal.set_text(print_left_str, 1)
-    matrixportal.set_text(progress_bar, 2)
-    matrixportal.scroll_text(SCROLL_DELAY)
+        # Update text
+        matrixportal.set_text(now_printing_str, 0)
+        matrixportal.set_text(print_left_str, 1)
+        matrixportal.set_text(progress_bar, 2)
+        matrixportal.scroll_text(SCROLL_DELAY)
+
+    except Exception as e:
+        print("Error: " + str(e))
+        refresh_time = None
+        matrixportal.set_text(str(e), 0)
+        matrixportal.scroll_text(SCROLL_DELAY)
 
     time.sleep(1)
